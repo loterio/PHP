1fef5c20fcd13ad67f1e33d47a86e2f1bc1a1283
fabio loterio <vitorloterio@gmail.com>
2020-08-20 07:46:35 -0300

    new repo

---
+++ myself/assets/components/header.php
@@ -0,0 +1,35 @@
+<?php 
+
+function createNavbar() {
+    $links = func_get_args();
+    $a = null;                  // variável usada para definir qual link está ativo
+
+    echo "<nav><span>Navigate below</span><ul>";
+
+    for($x = 0; $x < count($links); $x++){
+        if(strpos($links[$x],'[') !== false){
+            $a = explode('[',$links[$x]);
+                echo "<li><a href='$a[0].php'>$a[0]</a></li>";
+            $a = explode(']',$a[1]); 
+            $dw = explode(',',$a[0]);
+
+            for($i = 0; $i < count($dw); $i++){
+                echo "<p>> $dw[$i]</p><br>";
+                echo "\n";
+            }  
+        }else{
+            if(strpos($links[$x],'(active)') !== false or strpos($links[$x],'(act)') !== false){
+                $a = explode('(',$links[$x]); 
+                echo "<li class='active'><a href='$a[0].php'>$a[0]</a></li>";
+                echo "\n";
+            }else{
+                echo "<li><a href='$links[$x].php'>$links[$x]</a></li>";
+                echo "\n";
+            }
+        }
+    }
+    echo "</ul></nav>";
+}
+
+
+?>
